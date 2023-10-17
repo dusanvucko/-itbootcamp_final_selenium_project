83fcb7f5a1111ca9e50b65e36d2ad9858c626943
Dusan Vuckovic <dusanvuckovic95@outlook.com>
2023-10-17 15:23:21 +0200

    Initial Commit

---
+++ src/main/java/tests/retryAnalyzer.java
@@ -0,0 +1,24 @@
+package tests;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class retryAnalyzer implements IRetryAnalyzer {
+
+    private int count = 0;
+
+    private int maxRetry = 3;
+
+
+
+
+    @Override
+    public boolean retry(ITestResult iTestResult) {
+        if (count < maxRetry) {
+            count++;
+            return true;
+        }
+
+        return false;
+    }
+}
